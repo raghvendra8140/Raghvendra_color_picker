e10d13cf427b2b91e0e682165f19d20f5be73ac6
raghvendra8140 <vinodsinghvinodsingh863@gmail.com>
2024-08-29 13:42:03 +0530

    as a student of rai

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("colorPicker").addEventListener("input", function() {
+    let hexValue = this.value;
+    document.getElementById("hexValue").textContent = hexValue;
+
+    let r = parseInt(hexValue.slice(1, 3), 16);
+    let g = parseInt(hexValue.slice(3, 5), 16);
+    let b = parseInt(hexValue.slice(5, 7), 16);
+
+    let rgbValue = rgb(${r}, ${g}, ${b});
+    document.getElementById("rgbValue").textContent = rgbValue;
+});
